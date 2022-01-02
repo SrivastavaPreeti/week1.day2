d25be3671db4046ead4145e1ae020ad2b28cd492
Preeti Srivastava <95173525+SrivastavaPreeti@users.noreply.github.com>
2022-01-02 15:59:58 +0530

    Add files via upload

---
+++ signConversation.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class signConversation {
+
+	public static void main(String[] args) {
+		
+		int n = -40;
+		
+		if(n<0)
+			
+		{	
+		System.out.println("The number "+ n + " is converted to 40.");
+		}
+		
+		
+	}
+	
+	
+
+}
