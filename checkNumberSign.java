96b4c9cfc58867a7a437b5037fd7092b9bed959e
Preeti Srivastava <95173525+SrivastavaPreeti@users.noreply.github.com>
2022-01-02 16:18:31 +0530

    Add files via upload

---
+++ checkNumberSign.java
@@ -0,0 +1,25 @@
+package week1.day2;
+
+public class checkNumberSign {
+
+	public static void main(String[] args) {
+		 int n = 35;
+		 
+		 if (n>=0)
+		 {
+			 System.out.println("The number is Postive");
+			 
+		 }
+		 
+		 else if(n<0)
+		 {
+			 System.out.println("The number is negative");
+		 }
+		 
+		 else
+		 {
+			 System.out.println("The number is neither positive nor negative");
+		 }
+		 }
+	
+}		 
